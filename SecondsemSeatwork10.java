5689ff0019bc26a3c27978e1a4a1c38a6c8518ad
jerseyjeffrey <jeffreyjersey17.scc@gmail.com>
2026-02-27 05:27:19 +0000

    done

--- SecondsemSeatwork10.java
+++ SecondsemSeatwork10.java
@@ -1,1 +1,12 @@
-
+public class SecondsemSeatwork10 {
+	public static void main(String[] args) {
+    		try{ 
+            		int[] x = {1,2};
+                    		System.out.println(x[5]);
+                            		}catch (ArithmeticException e) {
+                                    			System.out.println("Math Error");
+                                                		} catch (ArrayIndexOutOfBoundsException e) {
+                                                        			System.out.println("Array error");
+                                                                    		}
+                                                                            	}
+                                                                                }
