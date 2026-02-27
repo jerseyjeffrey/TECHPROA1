0a6cbddeb75d1faf87af0839a00cd02304b4204a
jerseyjeffrey <jeffreyjersey17.scc@gmail.com>
2026-02-27 04:54:43 +0000

    done

--- SecondsemSeatwork7part1.java
+++ SecondsemSeatwork7part1.java
@@ -1,1 +1,14 @@
-
+public class SecondsemSeatwork7part1 {
+        public static void main(String[] args) {
+                int number1 = 20; 
+                        int number2 = 0;
+                                try {
+                                            int result = number1 / number2;
+                                                        System.out.println("Result: " + result); 
+                                                                } catch (ArithmeticException e) {
+                                                                            System.out.println("Built-in Exception: Cannot divide by zero"); 
+                                                                                    }
+                                                                                            System.out.println("Program continues running..."); 
+                                                                                                }
+                                                                                                }
+                                                                                            
