d10b120881d5e3894f0b27fb5aa8500732ab02ea
jerseyjeffrey <jeffreyjersey17.scc@gmail.com>
2026-03-03 05:46:51 +0000

    done

--- Sem2task3.java
+++ Sem2task3.java
@@ -1,1 +1,18 @@
+import java.util.Random;
 
+public class Sem2task3 {
+    public static void main(String[] args) {
+
+        Random random = new Random();
+
+        boolean isHeads;
+
+        isHeads = random.nextBoolean();
+
+        if (isHeads) {
+            System.out.println("HEADS");
+        } else {
+            System.out.println("TAILS");
+        }
+    }
+}
