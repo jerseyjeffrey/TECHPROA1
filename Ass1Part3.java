97b8249a7b60ff4c6bec886966558c8818d467a2
jerseyjeffrey <jeffreyjersey17.scc@gmail.com>
2026-03-03 04:58:22 +0000

    done

--- Ass1Part3.java
+++ Ass1Part3.java
@@ -1,1 +1,15 @@
+import javax.swing.*;
 
+public class Ass1Part1  {
+
+    public static void main(String[] args) {
+
+                JFrame frame = new JFrame("My First Swing Window");
+
+                                    frame.setSize(400, 300);
+
+                                                                frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+                                                                                                    frame.setVisible(true);
+                                                                                                                                            }
+                                                                                                                                                                                    }
